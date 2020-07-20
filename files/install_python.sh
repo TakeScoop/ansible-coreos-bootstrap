6b67d61f83dbf0b1f7581da44f6fce4004847f6d
Bo Huang <beyondbill@users.noreply.github.com>
2020-07-20 15:54:27 -0700

    use the latest pypy (#2)

--- files/install_python.sh
+++ files/install_python.sh
@@ -10,14 +10,14 @@ mkdir -p "$PYTHON_DIR"
 cd "$PYTHON_DIR"
 
 
-pypyFile="pypy$PYTHON_VERSION-$PYPY_VERSION-linux_x86_64-portable"
+pypyFile="pypy$PYTHON_VERSION-v$PYPY_VERSION-linux64"
 tarFile="$PYTHON_DIR/$pypyFile.tar.bz2"
 
 if [[ -e "$tarFile" ]]; then
   tar -xjf "$tarFile"
   rm -rf "$tarFile"
 else
-  curl -L "https://bitbucket.org/squeaky/portable-pypy/downloads/$pypyFile.tar.bz2" | tar -xjf -
+  curl -L "https://bitbucket.org/pypy/pypy/downloads/$pypyFile.tar.bz2" | tar -xjf -
 fi
 
 mv -n "$pypyFile" pypy
@@ -27,7 +27,7 @@ mkdir -p "$PYTHON_DIR/bin/"
 
 cat > "$PYTHON_DIR/bin/python" <<EOF
 #!/bin/bash
-LD_LIBRARY_PATH=$PYTHON_DIR/pypy/lib:$LD_LIBRARY_PATH exec $PYTHON_DIR/pypy/bin/pypy "\$@"
+LD_LIBRARY_PATH=$PYTHON_DIR/pypy/lib:$LD_LIBRARY_PATH exec $PYTHON_DIR/pypy/bin/pypy3 "\$@"
 EOF
 
 chmod +x "$PYTHON_DIR/bin/python"
